0af68111ace14278fea8f28116f67f1833ec4d7d
Irshad Sheikh <irshad.mike@gmail.com>
2018-01-16 02:02:23 +0530

    added apply methods

--- src/app/modules/bookmark/log-bookmark.controller.js
+++ src/app/modules/bookmark/log-bookmark.controller.js
@@ -5,26 +5,16 @@ export default
     constructor($scope, bookmarkService) {
         var _this = this;
         _this.bookMarks = [];
-        _this.parentBookMarks = [{id: "ddsdsd__1212", title:"parent bookmarks"}];
-        _this.getFolderLength = function(obj) {
+        _this.parentBookMarks = [];
+        _this.getFolderLength = function (obj) {
             return bookmarkService.getChildrenFolderLength(obj);
         }
 
         var getBookMarks = function () {
             bookmarkService.getParentBookMarks().then(
-                (arr) => {
-                    console.log("parents ", arr);
-                    _this.parentBookMarks = arr;
-                }
-            );
-            bookmarkService.getBookMarkFlatArray()
-                .then(
-                (arr) => {
-
-                    _this.bookMarks = arr;
-                    console.log("--", _this.bookMarks);
-                }
-                );
+                (arr) => { $scope.$apply(() => { _this.parentBookMarks = arr; }); });
+            bookmarkService.getBookMarkFlatArray().then(
+                (arr) => {$scope.$apply(() => { _this.bookMarks = arr; });});
         }
 
         getBookMarks();
